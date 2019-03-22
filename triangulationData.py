f79b6623a81d5f538fc98851b1dfe65b241a6713
bryantlta <bryantl@berkeley.edu>
2019-03-21 22:06:25 -0700

    added SPACE OBJECT

--- triangulationData.py
+++ triangulationData.py
@@ -1,15 +1,5 @@
 import csv
 
-class TriangleObject:
-    """ A Triangle Object includes information about each triangle, including
-    the three stars in the triangle, the angles and the distance in between them
-    . """
-
-    def __init__(self):
-        self.starA, self.starB, self.starC = None, None, None
-        self.distanceAB, self.distanceAC, self.distanceBC = 0, 0, 0
-        self.angleAB, self.angleAC, self.angleBC = 0, 0, 0
-
 class StarObject:
     """ A Star Object includes information about each star including name,
     position in 'ra' and position in 'dec,' and visual magnitude. """
@@ -20,8 +10,25 @@ class StarObject:
         self.positionDec = 0
         self.vMag = 0
 
+class TriangleObject:
+    """ A Triangle Object includes information about each triangle, including
+    the three stars in the triangle, the angles and the distance in between them
+    . """
+
+    def __init__(self):
+        self.starA, self.starB, self.starC = None, None, None
+        self.distanceAB, self.distanceAC, self.distanceBC = 0, 0, 0
+        self.angleAB, self.angleAC, self.angleBC = 0, 0, 0
+
+class SpaceObject:
+    """ A space object contians information about all stars. """
+
+    def __init__(self):
+        self.stars = []
+        self.triangles = []
+
 def importFile(file):
-    """ Imports a CSV file through the use of CSV. Returns a read file. """
+    """ Imports the file through CSV. Outputs the read file. """
 
     f = open(file)
     csv_f = csv.reader(f)
