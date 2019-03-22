355e9f81c2f36d846bbebacf8a27e2375fb148b4
bryantlta <bryantl@berkeley.edu>
2019-03-21 21:59:06 -0700

    defined triangle and star objects

---
+++ triangulationData.py
@@ -0,0 +1,15 @@
+import csv
+
+class TriangleObject:
+    """ A Triangle Object includes information about each triangle, including
+    the three stars in the triangle, the angles and the distance in between them
+    . """
+
+    def __init__(self):
+
+
+class StarObject:
+    """ A Star Object includes information about each star including name,
+    position in 'ra' and position in 'dec,' and visual magnitude. """
+
+    def __init__(self):
