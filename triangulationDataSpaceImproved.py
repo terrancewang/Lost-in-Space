f76b7c5752dc78eb7f4314376fa81031b7d39751
bryantlta <bryantl@berkeley.edu>
2019-03-22 23:28:32 -0700

    added a new file with an improved space complexity

---
+++ triangulationDataSpaceImproved.py
@@ -0,0 +1,182 @@
+import csv
+import math
+import pickle
+import import sqlite3
+from sqlite3 import Error
+
+"""
+Info:
+# A more complex definition of absolute magnitude is used for planets and small
+Solar System bodies, based on its brightness at one astronomical unit from the
+observer and the Sun. The Sun has an apparent magnitude of −27 and Sirius, the
+brightest visible star in the night sky, −1.46. This database is reduced so
+that it only holds the magnitude and coordinates of the stars with a visual
+magnitude below 5.3. Selecting the first 2300 brightest stars.
+
+Attribution Information:
+# The core projects were primarily created by Bryant Le (bryantl@berkeley.edu).
+Algorithm was developed by Tjorven Delabie, Thomas Durt, Jeroen Vandersteen.
+"""
+
+def importFile(file):
+    """ Imports the file through CSV. Outputs the read file. """
+
+    f = open(file)
+    csv_f = csv.reader(f)
+    return csv_f
+
+def parseStars(file):
+    """ Takes in a file containing information about stars and parses the file
+    into the constructor for the STAR object. Return a list of stars. """
+
+    initial = False
+    starList = []
+
+    def coordinateToDegree(degree, minute, second):
+        """ Helper function that calculates the position in terms of decimal
+        from coordinate system of degrees, minutes, and seconds. """
+
+        degree = float(degree) + float(minute) / 60
+        degree = degree + float(second) / 3600
+        return degree
+
+    def extractId(name):
+        """ Helper function that extracts the ID from the name and returns the
+        integer form. """
+
+        stringID = ''
+        for i in name:
+            if i in '0123456789':
+                stringID = stringID + i
+        stringID = int(stringID)
+        return stringID
+
+    for row in file:
+        if initial == False:
+            initial = True
+        else:
+            name, ra1, ra2, ra3, dec1, dec2, dec3, vmag = row
+            star = [name, coordinateToDegree(ra1, ra2, ra3), \
+                coordinateToDegree(dec1, dec2, dec3), vmag]
+            starList.append(star)
+    return starList
+
+def euclideanDistance(starA, starB):
+    """ Computes the euclidean distance between starA and starB and returns
+    the distance."""
+
+    raDiff = starA[1] - starB[1]
+    decDiff = starA[2] - starB[2]
+    distance = (raDiff ** 2 + decDiff ** 2) ** 0.5
+    return distance
+
+def distances(starA, starB, starC):
+    """ Computes euclidean distance between each of the stars. Returns a list
+    of euclidean distances between each star. """
+
+    distAB = euclideanDistance(starA, starB)
+    distAC = euclideanDistance(starA, starC)
+    distBC = euclideanDistance(starB, starC)
+    distances = [distAB, distAC, distBC]
+    return distances
+
+def angles(distAB, distAC, distBC):
+    """ Computes the angles of the triangulation of the three stars using the
+    Law of Cosine. """
+
+    cosA = (distAB ** 2 + distAC ** 2 - distBC ** 2) / (2 * distAB * distAC)
+    angleA = math.acos(cosA)
+    cosB = (distAB ** 2 + distBC ** 2 - distAC ** 2) / (2 * distAB * distBC)
+    angleB = math.acos(cosB)
+    cosC = (distAC ** 2 + distBC ** 2 - distAB ** 2) / (2 * distAC * distBC)
+    angleC = math.acos(cosC)
+    angles = [angleA, angleB, angleC]
+    return angles
+
+def constructTriangle(starA, starB, starC):
+    """ Constructs a triangle, determines distances between stars, angles
+    between stars, and returns a TRIANGLE object. """
+    triangle = [starA[0], starB[0], starC[0]] + distances(starA, starB, starC) \
+        + [starA[3], starB[3], starC[3]]
+    return triangle
+
+def constructTriangles(starList):
+    """ Constructs TRIANGLE object from a list of STAR objects from a SPACE
+    object. Return a list of TRIANGLE objects. """
+    i = -1
+    for starA in starList:
+        i += 1
+        j = i
+        for starB in starList[i + 1: len(starList) + 1]:
+            j += 1
+            print(i,j)
+            for starC in starList[j + 1 : len(starList) + 1]:
+                triangle = constructTriangle(starA, starB, starC)
+    return triangles
+
+def sortByAngleSum(triangleList):
+    def partition(triangleList,low,high):
+        i = low - 1
+        pivot = triangleList[high].angleDiffSum
+        for j in range(low , high):
+            if triangleList[j].angleDiffSum <= pivot:
+                i = i + 1
+                triangleList[i], triangleList[j] = triangleList[j], triangleList[i]
+        triangleList[i+1], triangleList[high] = triangleList[high], triangleList[i+1]
+        return i + 1
+
+    def quickSort(triangleList, low, high):
+        if low < high:
+            pi = partition(triangleList, low, high)
+            quickSort(triangleList, low, pi-1)
+            quickSort(triangleList, pi+1, high)
+
+    quickSort(triangleList, 0, len(triangleList)-1)
+
+def getAngleSum(triangleList):
+    angleSumList = []
+    for triangle in triangleList:
+        angleSumList.append(triangle.angleDiffSum)
+    return angleSumList
+
+def exportPickle(triangleList):
+    with open("triangleAngleSum.txt", "wb") as fp:
+        angleSumList = getAngleSum(triangleList)
+        pickle.dump(angleSumList, fp)
+
+def constructTable():
+    def createConnection(dbFile):
+
+def exportCSV(triangleList):
+    """ Export the CSV file of triangle objects. """
+
+    with open('customerOrderAverages.csv', 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',',quotechar='|',\
+            quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow(['Star A', 'Star B', 'Star C', 'Distance AB', \
+            'Distance AC', 'Distance BC', 'Angle A', 'Angle B', 'Angle C', \
+            'Angle Sum', 'Magnitude A', 'Magnitude B', 'Magnitude C'])
+        for triangle in triangleList:
+            starA = str(triangle.starA.name)
+            starB = str(triangle.starB.name)
+            starC = str(triangle.starC.name)
+            distAB = str(triangle.distAB)
+            distAC = str(triangle.distAC)
+            distBC = str(triangle.distBC)
+            angleA = str(triangle.angleA)
+            angleB = str(triangle.angleB)
+            angleC = str(triangle.angleC)
+            angleSum = str(triangle.angleDiffSum)
+            magA = str(triangle.magA)
+            magB = str(triangle.magB)
+            magC = str(triangle.magC)
+            filewriter.writerow([starA, starB, starC, distAB, distAC, distBC, \
+                angleA, angleB, angleC, angleSum, magA, magB, magC])
+
+if __name__ == "__main__":
+    file = importFile('Star Data - Sheet2.csv')
+    starList = parseStars(file)
+    triangleList = constructTriangles(starList)
+    #sortedTriangleList = sortByAngleSum(triangleList)
+    #exportPickle(sortedTriangleList)
+    #exportCSV(sortedTriangleList)
